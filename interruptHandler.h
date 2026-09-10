9473669a76ccd46284d171f450aa8d195cc61d45
FaikyFigueiredo <contato.fabiohenriquefigueiredo@gmail.com>
2026-09-10 06:23:50 -0300

    feat: Priority and function added to lead better with interruptions, also instead of exti line and port we have the struct pin

--- interruptHandler.h
+++ interruptHandler.h
@@ -1,19 +1,42 @@
 #ifndef INTERRUPT_HANDLER_H
 #define INTERRUPT_HANDLER_H
 
-#include "stm32f4xx_hal.h"
+#include "gpioHandler.h"
+
+typedef void (*function)(void);
+
+static volatile function ISRs[7];
+
+//Functions to handle with interruptions
+void EXTI0_IRQHanlder(void);
+
+void EXTI1_IRQHanlder(void);
+
+void EXTI2_IRQHanlder(void);
+
+void EXTI3_IRQHanlder(void);
+
+void EXTI4_IRQHanlder(void);
+
+void EXTI9_5_IRQHanlder(void);
+
+void EXTI15_10_IRQHanlder(void);
+
+
 
 void interruptiOn(void);
 /*
 Initiate SYSCFG clock
 */
 
-void interruptPin(GPIO_TypeDef* porta, uint8_t line, uint8_t edge);
+void interruptPin(Pin* pin, uint8_t edge, uint8_t priority, function func);
 /*
 Set a pin to interruption
 @Param porta-> GPIO port to use
 @Param line-> EXTI line to use
 @Param edge-> set the edge to detect(values: 0->Falling edge, 1->Rising Edge, 2->Both)
+@Param priority-> set the priority of the interruption
+@Param func-> set the function to handle with the interruption
 */
 
 #endif
