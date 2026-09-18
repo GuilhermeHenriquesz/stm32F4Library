56cbeb9cd9ca2dbb3f81e4a5b88db08ab4e5ae16
FaikyFigueiredo <contato.fabiohenriquefigueiredo@gmail.com>
2026-09-17 23:47:02 -0300

    fix: now the function writeInDisplay use the mask correctly

--- sevenSegmentsHandler.c
+++ sevenSegmentsHandler.c
@@ -2,7 +2,7 @@
 
 void writeInDisplay(Pin* pins, uint8_t number){
     for(uint8_t = 0; i<7 ; i++)
-        if((getPinStatus(&pins[i]) << i) != (number & (0b1 << i)))
+        if((getPinStatus(&pins[i]) << i) != (number & (0b1 << 6 - i)))
             togglePin(&pins[i]);
 
 }
