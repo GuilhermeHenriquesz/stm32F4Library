133f08324b2a0106fc8b406a5c03ffb65382feed
Guilherme Henriques <gui.henriques500@gmail.com>
2026-09-25 13:43:54 -0300

    Fix display logic in writeInDisplay function

--- sevenSegmentsHandler.c
+++ sevenSegmentsHandler.c
@@ -2,7 +2,7 @@
 
 void writeInDisplay(Pin* pins, uint8_t number){
     for(uint8_t i = 0; i<7 ; i++)
-        if((getPinStatus(&pins[i]) << i) != (number & (0b1 << 6 - i)))
+        if((getPinStatus(&pins[i]) << 6 - i) != (~number & (0b1 << (6 - i))))
             togglePin(&pins[i]);
 
 }
